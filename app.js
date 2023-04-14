f244b47c8d7e6ccf675243661f922698552b3bb6
amilpa <amilpa2020@gmail.com>
2023-04-14 19:17:31 +0530

    added routing and middleware

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 
-console.log('Hello world')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+
+require('dotenv').config()
+
+//middleware
+
+app.use(express.json())
+
+//routes
+
+app.use('/api/v1/tasks',tasks)
+
+
+const port = process.env.PORT || 3000
+
+const serverStart = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)    
+    app.listen(3000, () => {
+      console.log(`server listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+serverStart()
