93cc1c0027adab3b0fe7779f511b09cdf3936108
amilpa <amilpa2020@gmail.com>
2023-04-15 10:48:47 +0530

    setup models

--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const taskSchema = mongoose.Schema({
-  taskName : String,
-  completed : Boolean
+  taskName : {
+    type : String,
+    required : true,
+    maxLength : [20,'Maximum length is 20'],
+    trim: true,
+  },
+  completed :{
+    type : Boolean, 
+    default : false
+  }
 })
 
 module.exports = mongoose.model('Tasks' , taskSchema)
